0b96c1352de8e68a8c9bffbbd4d36609bd0a76c9
mesutKoc <mkoc1367@gmail.com>
2024-01-04 01:45:55 +0100

    first commit

---
+++ app/src/main/java/com/example/androidprojectartbookjava/ArtActivity.java
@@ -0,0 +1,29 @@
+package com.example.androidprojectartbookjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class ArtActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_art);
+    }
+
+    public void save (View view) {
+
+
+    }
+
+    public void selectImage (View view) {
+
+
+    }
+
+
+
+
+}
